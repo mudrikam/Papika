6f40de4e4514a1013819132cb5206a198accc16b
mudrikan <mudrikulh@gmail.com>
2026-01-08 20:03:49 +0700

    Add relaunch menu item and enhance sidebar with action buttons for embedding, captioning, and hashing

--- UI/Widgets/SideBarWidget/sidebar_action_widget.py
+++ UI/Widgets/SideBarWidget/sidebar_action_widget.py
@@ -1,11 +1,40 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton
 from PySide6.QtCore import Qt
+import qtawesome as qta
 
 class SidebarActionWidget(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
         layout = QVBoxLayout(self)
         layout.setContentsMargins(8, 8, 8, 8)
-        label = QLabel('this will be filled witch actions tools')
-        label.setAlignment(Qt.AlignCenter)
+        layout.setSpacing(6)
+        label = QLabel('Step 1 Generate Embedding')
+        label.setAlignment(Qt.AlignLeft)
         layout.addWidget(label)
+        generate_embedding_button = QPushButton('Generate Embedding', self)
+        generate_embedding_button.setObjectName('generate_embedding_button')
+        generate_embedding_button.setIcon(qta.icon('fa6s.wand-magic-sparkles'))
+        generate_embedding_button.setCursor(Qt.PointingHandCursor)
+        generate_embedding_button.setFixedHeight(36)
+        layout.addWidget(generate_embedding_button)
+
+        label2 = QLabel('Step 2 Generate Image Caption')
+        label2.setAlignment(Qt.AlignLeft)
+        layout.addWidget(label2)
+        generate_caption_button = QPushButton('Generate Caption', self)
+        generate_caption_button.setObjectName('generate_caption_button')
+        generate_caption_button.setIcon(qta.icon('fa6s.image'))
+        generate_caption_button.setCursor(Qt.PointingHandCursor)
+        generate_caption_button.setFixedHeight(36)
+        layout.addWidget(generate_caption_button)
+
+        label3 = QLabel('Step 3 Generate Image Hash')
+        label3.setAlignment(Qt.AlignLeft)
+        layout.addWidget(label3)
+        generate_hash_button = QPushButton('Generate Image Hash', self)
+        generate_hash_button.setObjectName('generate_hash_button')
+        generate_hash_button.setIcon(qta.icon('fa6s.hashtag'))
+        generate_hash_button.setCursor(Qt.PointingHandCursor)
+        generate_hash_button.setFixedHeight(36)
+        layout.addWidget(generate_hash_button)
+        layout.addStretch()
--- UI/Widgets/menu_bar_widget.py
+++ UI/Widgets/menu_bar_widget.py
@@ -8,43 +8,69 @@ from UI.Dialogs.about_dialog import show_about
 from UI.Dialogs.license_dialog import show_license
 import webbrowser
 from Configs.configs_file_manager import load_config
+import os
+import sys
 
 class MenuBar(QMenuBar):
     def __init__(self, menu_cfg: dict, base_path: Path, parent=None):
         super().__init__(parent)
         self.base_path = base_path
         self.links = load_config(base_path).get("links", {})
+        self._setup_role_handlers()
         for mcfg in menu_cfg.get("menus", []):
-            menu = QMenu(mcfg["title"], self)
-            for item in mcfg.get("items", []):
-                label = item["label"]
-                icon = None
-                icon_name = item.get("icon")
-                if icon_name:
-                    if icon_name.startswith("fa"):
-                        icon = qta.icon(icon_name)
-                    else:
-                        icon = get_icon(icon_name, base_path)
-                act = QAction(icon, label, self) if icon else QAction(label, self)
-                shortcut = item.get("shortcut")
-                if shortcut:
-                    act.setShortcut(shortcut)
-                role = item.get("role")
-                if role == "quit":
-                    act.triggered.connect(QApplication.quit)
-                elif role == "about":
-                    act.triggered.connect(lambda checked=False: show_about(self.parentWidget(), self.base_path))
-                elif role == "license":
-                    act.triggered.connect(lambda checked=False: show_license(self.parentWidget(), self.base_path))
-                elif role == "whatsapp":
-                    act.triggered.connect(lambda checked=False: webbrowser.open(self.links.get("whatsapp")))
-                elif role == "tiktok":
-                    act.triggered.connect(lambda checked=False: webbrowser.open(self.links.get("tiktok")))
-                elif role == "readme":
-                    act.triggered.connect(lambda checked=False: webbrowser.open(self.links.get("readme")))
-                elif role == "telegram":
-                    act.triggered.connect(lambda checked=False: webbrowser.open(self.links.get("telegram")))
-                elif role == "repo":
-                    act.triggered.connect(lambda checked=False: webbrowser.open(self.links.get("repo")))
-                menu.addAction(act)
-            self.addMenu(menu)
+            if mcfg.get("direct_action"):
+                self._create_direct_action_menu(mcfg)
+            else:
+                self._create_submenu(mcfg)
+
+    def _setup_role_handlers(self):
+        self.role_handlers = {
+            "quit": QApplication.quit,
+            "relaunch": self._relaunch_app,
+            "about": lambda: show_about(self.parentWidget(), self.base_path),
+            "license": lambda: show_license(self.parentWidget(), self.base_path),
+            "whatsapp": lambda: webbrowser.open(self.links.get("whatsapp")),
+            "tiktok": lambda: webbrowser.open(self.links.get("tiktok")),
+            "readme": lambda: webbrowser.open(self.links.get("readme")),
+            "telegram": lambda: webbrowser.open(self.links.get("telegram")),
+            "repo": lambda: webbrowser.open(self.links.get("repo"))
+        }
+
+    def _get_icon(self, icon_name: str):
+        if not icon_name:
+            return None
+        if icon_name.startswith("fa"):
+            return qta.icon(icon_name)
+        return get_icon(icon_name, self.base_path)
+
+    def _create_action(self, label: str, icon_name: str = None, shortcut: str = None, role: str = None):
+        icon = self._get_icon(icon_name)
+        action = QAction(icon, label, self) if icon else QAction(label, self)
+        if shortcut:
+            action.setShortcut(shortcut)
+        if role and role in self.role_handlers:
+            action.triggered.connect(self.role_handlers[role])
+        return action
+
+    def _create_submenu(self, menu_config: dict):
+        menu = QMenu(menu_config["title"], self)
+        for item in menu_config.get("items", []):
+            action = self._create_action(
+                label=item["label"],
+                icon_name=item.get("icon"),
+                shortcut=item.get("shortcut"),
+                role=item.get("role")
+            )
+            menu.addAction(action)
+        self.addMenu(menu)
+
+    def _create_direct_action_menu(self, menu_config: dict):
+        action = self._create_action(
+            label=menu_config["title"],
+            icon_name=menu_config.get("icon"),
+            role=menu_config.get("role")
+        )
+        self.addAction(action)
+
+    def _relaunch_app(self):
+        os.execv(sys.executable, [sys.executable] + sys.argv)
